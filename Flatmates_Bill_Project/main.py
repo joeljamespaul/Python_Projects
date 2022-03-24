fa282b2607a44e37697fbc0f27a16018c11aaa04
joeljamespaul <joeljamespaul1996@gmail.com>
2022-03-24 16:22:26 +0530

    Project: Flatmates_Bill spelling mistakes

--- Flatmates_Bill_Project/main.py
+++ Flatmates_Bill_Project/main.py
@@ -2,13 +2,13 @@ from flat import Bill, Flatmate
 from pdf_generator import PdfGenerator,FileShare
 
 period = input("Enter the time period of stay: ")
-total_expense = float(input("Enter the total bill for the period of {} :".format(period)))
+total_expense = float(input("Enter the total bill for the period of {} (e.g:March 2020) :".format(period)))
 the_bill = Bill(total_expense=total_expense, period=period)
 
 flatmate1_name = input("Enter the name of the first_flatmate: ")
-flatmate1_days_in_house = int(input("Enter the number of days {} spent int the house: ".format(flatmate1_name)))
+flatmate1_days_in_house = int(input("Enter the number of days {} spent in the house: ".format(flatmate1_name)))
 flatmate2_name = input("Enter the second of the first_flatmate: ")
-flatmate2_days_in_house = int(input("Enter the number of days {} spent int the house: ".format(flatmate2_name)))
+flatmate2_days_in_house = int(input("Enter the number of days {} spent in the house: ".format(flatmate2_name)))
 flatmate1 = Flatmate(flatmate1_name, flatmate1_days_in_house)
 flatmate2 = Flatmate(flatmate2_name, flatmate2_days_in_house)
 
