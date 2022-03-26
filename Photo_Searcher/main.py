fc872bd07258ac9d30ec4b7a98ae0a6f94c312c7
joeljamespaul <joeljamespaul1996@gmail.com>
2022-03-27 00:24:35 +0530

    Project: Photo_Searcher undid some changes which broke the code

--- Photo_Searcher/main.py
+++ Photo_Searcher/main.py
@@ -11,33 +11,20 @@ class FirstScreen(Screen):
     def search_image(self):
         # Get text input from the user
         query = self.manager.current_screen.ids.text_input.text
-        return query
-
-    def download_image(self):
-        """
-        Downloads the image from wikipedia as per the user's query
-        :return: image.jpg file path
-        """
-        query = self.search_image()
         # Get wikipedia page and get the first image link from list of image links
         page = wikipedia.page(query)
         image_link = page.images[0]
         # Download the image using requests.get() and
         # write the content(binary data) of the request object to the empty jpg file
         # Note: write as wb as the data is of binary form
-        # added header to avoid 403 error from get requests
+        # added header to avoud 403 error from get requestss
         headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                                  'Chrome/99.0.4844.82 Safari/537.36'}
         req = requests.get(url=image_link, headers=headers)
-        image_path = 'files/image.jpg'
-        with open(image_path, 'wb') as file:
+        with open('files/image.jpg', 'wb') as file:
             file.write(req.content)
-        return image_path
-
-    def set_image(self):
-        image_path = self.download_image()
         # Set the image in the image widget
-        self.manager.current_screen.ids.img.source = image_path
+        self.manager.current_screen.ids.img.source = 'files/image.jpg'
 
 
 class RootWidget(ScreenManager):
