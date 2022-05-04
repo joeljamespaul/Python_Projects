75b2e4d1b1b102b536d9880ebe0a43706b69f417
joeljamespaul <joeljamespaul1996@gmail.com>
2022-05-05 01:17:56 +0530

    added empty classes and imported flask and Flatmates_Bill_Project terminal version

---
+++ Flatmates_Bill_Webapp/main.py
@@ -0,0 +1,18 @@
+from flask.views import MethodView
+from wtforms import form
+
+
+class HomePage(MethodView):
+    pass
+
+
+class BillFormPage(MethodView):
+    pass
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(form):
+    pass
